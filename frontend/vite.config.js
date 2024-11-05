2eb55df48e2ca54d78073e779a96253a902a6b3c
Abhishek Tripathi <ronalabhishek@gmail.com>
2024-11-05 15:49:25 +0530

    #

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
+  server:{
+    proxy:{
+      '/api':{
+        target:'http://localhost:3000',
+        secure:false
       }
     }
-  },
-  build: {
-    outDir: 'dist', // Ensure the output directory is explicitly set to 'dist'
   }
 })
